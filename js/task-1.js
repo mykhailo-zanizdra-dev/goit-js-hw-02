fd503ea0289b63ab8345809ae06f0f7ff3f07648
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-06-07 11:30:12 +0300

    fix: task 1 fix.

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   let totalPrice = quantity * pricePerDroid;
 
   if (totalPrice <= customerCredits) {
-    return `You ordered ${quantity}  droids worth ${totalPrice} credits!`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   } else {
     return "Insufficient funds!";
   }
